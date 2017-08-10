3a5d0389cc86870f28fb2bb1829ee753054dee27
Iwata Hidetaka <iwata0303@gmail.com>
2017-08-10 10:57:16 +0900

    Relocate type declarations.
    Declare infrastructure types in base.d.ts and refer it by /// reference.
    Move *.d.ts into ./types.
    Now, copying base.d.ts to ./lib is not needed.

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,14 @@
-import "./base";
+///<reference path="../types/base.d.ts" />
 import Vue from "vue";
 
-export type Constructor<T> = new (...args: any[]) => T;
-
-export type PropType = Constructor<any> | Constructor<any>[] | null;
-export type PropsDefinition<PropKeys extends string> = {
-    [K in PropKeys]: Vue.PropOptions | PropType;
-};
-export type ComponentOptions<Props> = Vue.ComponentOptions<Vue> & {
-    props?: PropsDefinition<keyof Props> | string[]
-};
-
-export type EventHandlers<E> = {
-    [K in keyof E]?: (payload: E[K]) => void;
-}
-
-export interface ElementEvents {
-    click: Event;
-    dblclick: Event;
-    keydown: KeyboardEvent;
-    keyup: KeyboardEvent;
-    keypress: KeyboardEvent;
-    // TODO: Add other events
-}
-
-export interface ElementEventsNativeOn {
-    nativeOnClick: Event;
-    nativeOnDblclick: Event;
-    nativeOnKeydown: KeyboardEvent;
-    nativeOnKeyup: KeyboardEvent;
-    nativeOnKeypress: KeyboardEvent;
-    // TODO: Add other events
-}
-
-export type TsxComponentAttrs<Props, Events> = (
-    { props: Partial<Props> } &
-    Partial<Props> &
-    Vue.VNodeData &
-    EventHandlers<Events> &
-    EventHandlers<ElementEventsNativeOn> &
-    VueTsxSupport.AddtionalAttrs
-) | (
-    Props &
-    Vue.VNodeData &
-    EventHandlers<Events> &
-    EventHandlers<ElementEventsNativeOn> &
-    VueTsxSupport.AddtionalAttrs
-);
-
-export type TsxComponent<V extends Vue, Props, Events> = Constructor<V & {
-    _tsxattrs: TsxComponentAttrs<Props, Events>
+export type TsxComponent<V extends Vue, Props, Events> = VueTsx.Constructor<V & {
+    _tsxattrs: VueTsx.TsxComponentAttrs<Props, Events>
 }>;
 
 /**
  * Add TSX-support to existing component factory
  */
-export function convert<V extends Vue>(component: Constructor<V>): TsxComponent<V, any, {}> {
+export function convert<V extends Vue>(component: VueTsx.Constructor<V>): TsxComponent<V, any, {}> {
     return component as any;
 }
 
@@ -69,17 +22,17 @@ export function convert<V extends Vue>(component: Constructor<V>): TsxComponent<
  *  // Get TSX-supported component with props(`name`, `value`) and event(`onInput`)
  *  const NewComponent = tsx.of<{ name: string, value: string }, { onInput: string }>.convert(Component);
  */
-export function of<Props, Events = {}>(): { convert: <V extends Vue>(component: Constructor<V>) => TsxComponent<V, Props, Events> } {
+export function of<Props, Events = {}>(): { convert: <V extends Vue>(component: VueTsx.Constructor<V>) => TsxComponent<V, Props, Events> } {
     return { convert: ((c: Vue) => c) as any };
 }
 
 /**
  * Create component from component options (Compatible with Vue.extend)
  */
-export function createComponent<Props = any, Events = {}>(opts: ComponentOptions<Props> | Vue.FunctionalComponentOptions): TsxComponent<Vue, Props, Events> {
+export function createComponent<Props = any, Events = {}>(opts: VueTsx.ComponentOptions<Props> | Vue.FunctionalComponentOptions): TsxComponent<Vue, Props, Events> {
     return Vue.extend(opts) as any;
 }
 
 export class Component<Props = any, Events = {}> extends Vue {
-    _tsxattrs: TsxComponentAttrs<Props, Events>;
+    _tsxattrs: VueTsx.TsxComponentAttrs<Props, Events>;
 }
---
+++ types/base.d.ts
@@ -0,0 +1,67 @@
+import Vue from "vue";
+
+declare global {
+    namespace JSX {
+        interface Element extends Vue.VNode {
+        }
+
+        interface ElementClass extends Vue {
+        }
+
+        interface ElementAttributesProperty {
+            _tsxattrs: any;
+        }
+    }
+
+    namespace VueTsx {
+        interface AddtionalAttrs {
+        }
+
+        type Constructor<T> = new (...args: any[]) => T;
+
+        type PropType = Constructor<any> | Constructor<any>[] | null;
+        type PropsDefinition<PropKeys extends string> = {
+            [K in PropKeys]: Vue.PropOptions | PropType;
+        };
+        type ComponentOptions<Props> = Vue.ComponentOptions<Vue> & {
+            props?: PropsDefinition<keyof Props> | string[]
+        };
+
+        type EventHandlers<E> = {
+            [K in keyof E]?: (payload: E[K]) => void;
+        }
+
+        interface ElementEvents {
+            click: Event;
+            dblclick: Event;
+            keydown: KeyboardEvent;
+            keyup: KeyboardEvent;
+            keypress: KeyboardEvent;
+            // TODO: Add other events
+        }
+
+        interface ElementEventsNativeOn {
+            nativeOnClick: Event;
+            nativeOnDblclick: Event;
+            nativeOnKeydown: KeyboardEvent;
+            nativeOnKeyup: KeyboardEvent;
+            nativeOnKeypress: KeyboardEvent;
+            // TODO: Add other events
+        }
+
+        type TsxComponentAttrs<Props, Events = {}> = (
+            { props: Partial<Props> } &
+            Partial<Props> &
+            Vue.VNodeData &
+            EventHandlers<Events> &
+            EventHandlers<ElementEventsNativeOn> &
+            AddtionalAttrs
+        ) | (
+            Props &
+            Vue.VNodeData &
+            EventHandlers<Events> &
+            EventHandlers<ElementEventsNativeOn> &
+            AddtionalAttrs
+        );
+    }
+}
