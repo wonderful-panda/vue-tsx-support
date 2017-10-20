c175a6edbbbb46f591d748ede45d1d40d28446b3
Iwata Hidetaka <iwata0303@gmail.com>
2017-10-20 19:46:23 +0900

    add test abount enable-vue-router

---
+++ test/tsc/enable-vuerouter/test.tsx
@@ -0,0 +1,19 @@
+import "vue-tsx-support/enable-check";
+import * as vuetsx from "vue-tsx-support";
+import "vue-tsx-support/options/enable-vue-router";
+import Vue from "vue";
+
+const noop = () => {};
+const func = (_: KeyboardEvent) => {}
+
+/*
+ * Components written by standard way
+ */
+function standardComponent() {
+    const MyComponent = Vue.extend({});
+
+    <router-link to="." />;
+    <router-link to={{ name: "main" }} />;
+    // NG
+    <router-link to={{ name: 0 }} />;   //// TS2322: 'number' is not assignable
+}
