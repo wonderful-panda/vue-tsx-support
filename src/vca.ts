23fa92852e1fdaa4a50953a0bbda1fd63c4de5a7
iwata hidetaka <iwata0303@gmail.com>
2019-10-16 01:50:24 +0900

    vca: remove componentFactoryOf / add emit helper

--- src/vca.ts
+++ src/vca.ts
@@ -2,42 +2,66 @@ import Vue, { VNode } from "vue";
 import * as vca from "@vue/composition-api";
 import { _TsxComponentV3, RequiredPropNames, PropsForOutside } from "./api";
 import { RecordPropsDefinition } from "vue/types/options";
-import { InnerScopedSlots } from "../types/base";
+import {
+  InnerScopedSlots,
+  TsxComponentTypeInfo,
+  EventHandler,
+  DeclareOn,
+  DeclarePrefixedEvents,
+  DeclareProps
+} from "../types/base";
 
-export type SetupContext<ScopedSlots> = vca.SetupContext & {
+export type SetupContext<PrefixedEvents = {}, ScopedSlots = {}, On = {}> = vca.SetupContext & {
   slots: InnerScopedSlots<ScopedSlots>;
+  _tsx?: TsxComponentTypeInfo<{}, {}, PrefixedEvents, On>;
 };
 
 export interface CompositionComponentOptions<
   Props,
   PropsDef extends RecordPropsDefinition<Props>,
-  ScopedSlots
+  PrefixedEvents,
+  ScopedSlots,
+  On
 > {
   props?: PropsDef & RecordPropsDefinition<Props>;
-  setup: (this: void, props: Props, ctx: SetupContext<ScopedSlots>) => (() => VNode);
+  setup: (
+    this: void,
+    props: Props,
+    ctx: SetupContext<PrefixedEvents, ScopedSlots, On>
+  ) => (() => VNode);
 }
 
-export interface ComponentFactory<PrefixedEvents, ScopedSlots> {
-  create<
-    Props,
-    PropsDef extends RecordPropsDefinition<Props>,
-    RequiredProps extends keyof Props = RequiredPropNames<PropsDef> & keyof Props
-  >(
-    options: CompositionComponentOptions<Props, PropsDef, ScopedSlots>
-  ): _TsxComponentV3<Vue, {}, PropsForOutside<Props, RequiredProps>, PrefixedEvents, {}, {}>;
-}
-
-const factory = {
-  create(options: any) {
-    return vca.createComponent(options);
-  }
-} as ComponentFactory<{}, {}>;
-
-export const component = factory.create;
-
-export function componentFactoryOf<PrefixedEvents, ScopedSlots = {}>(): ComponentFactory<
+export function component<
+  Props,
+  PropsDef extends RecordPropsDefinition<Props>,
   PrefixedEvents,
+  ScopedSlots,
+  On,
+  RequiredProps extends keyof Props = RequiredPropNames<PropsDef> & keyof Props
+>(
+  options: CompositionComponentOptions<Props, PropsDef, PrefixedEvents, ScopedSlots, On>
+): _TsxComponentV3<
+  Vue,
+  {},
+  PropsForOutside<Props, RequiredProps>,
+  PrefixedEvents,
+  On,
   ScopedSlots
 > {
-  return factory as any;
+  return vca.createComponent(options as any) as any;
+}
+export function emit<Events, Name extends string & keyof Events>(
+  ctx: SetupContext<any, any, Events>,
+  name: Name,
+  ...args: Parameters<EventHandler<Events[Name]>>
+) {
+  ctx.emit(name, ...args);
+}
+
+export function emitOn<Events, Name extends string & keyof Events>(
+  ctx: SetupContext<Events, any, {}>,
+  name: Name,
+  ...args: Parameters<EventHandler<Events[Name]>>
+) {
+  ctx.emit(name.replace(/^on[A-Z]/, v => v[2].toLowerCase()), ...args);
 }
--- test/tsc/basic/vca.tsx
+++ test/tsc/basic/vca.tsx
@@ -1,4 +1,4 @@
-import { component, componentFactoryOf } from "vue-tsx-support/lib/vca";
+import { component, SetupContext, emit, emitOn } from "vue-tsx-support/lib/vca";
 
 const MyComponent = component({
   props: {
@@ -14,13 +14,26 @@ const MyComponent = component({
 <MyComponent bar />; // OK
 <MyComponent foo="a" />; //// TS2322 | TS2769: 'bar' is missing
 
-const MyComponent2 = componentFactoryOf<{ onCustomEvent: string }, { ss: boolean }>().create({
+const MyComponent2 = component({
   props: {
     foo: String
   },
-  setup(props, ctx) {
+  setup(
+    props,
+    ctx: SetupContext<{ onCutstomEvent: string }, { ss: boolean }, { customEvent: string }>
+  ) {
+    const onClick = () => {
+      emit(ctx, "customEvent", "value");
+      emitOn(ctx, "onCutstomEvent", "value");
+
+      emit(ctx, "customEvent2", "value"); //// TS2345
+      emitOn(ctx, "onCutstomEvent2", "value"); //// TS2345
+
+      emit(ctx, "customEvent", 1); //// TS2345
+      emitOn(ctx, "onCutstomEvent", 1); //// TS2345
+    };
     return () => <div class={props.foo}>{ctx.slots.ss(true)}</div>;
   }
 });
 
-<MyComponent2 foo="a" onCustomEvent={s => console.log(s.toUpperCase())} />; // OK
+<MyComponent2 foo="a" onCutstomEvent={v => console.log(v.toUpperCase())} />; // OK
