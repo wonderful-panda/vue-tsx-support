f45b74450316869716ebd9f6ce20dfaa5ce108f0
iwata hidetaka <iwata0303@gmail.com>
2020-11-17 20:32:37 +0900

    avoid export star from d.ts
    it breaks transpiled code when using TS >= 3.9

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export * from "./api";
-export * from "../types/v2-compat";
+export { TsxComponent, TsxComponentAttrs, TsxComponentInstance } from "../types/v2-compat";
 export { modifiers } from "./modifiers";
 export {
   DeclareProps,
